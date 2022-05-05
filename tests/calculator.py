80d77cfec29f4cdd98fbcdcca04c531a3445c9b0
yoshi <yn55@njit.edu>
2022-05-05 18:58:07 -0400

    Test 12. testing result of calculator

--- tests/calculator.py
+++ tests/calculator.py
@@ -6,3 +6,8 @@ def test_calculator_is_instance():
     """Testing the Calculator"""
     calculator = Calculator()
     assert isinstance(calculator, Calculator)
+
+def test_calculator_get_result_method():
+    """Testing the Calculator"""
+    calculator = Calculator()
+    assert calculator.get_result() == 0
