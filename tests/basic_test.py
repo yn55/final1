89c00ab31d9b6a65df5e682da17e26a9ea013628
yoshi <yn55@njit.edu>
2022-05-05 18:06:34 -0400

    Test 4. Testing login page in the Index Client

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -16,4 +16,10 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'href="/about"' in response.data
+    assert b'href="/welcome"' in response.data
+
+def test_request_main_menu_links(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b'href="/login"' in response.data
