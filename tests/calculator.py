6cacdee1ded019eb3dd3f1b635bcc6a55b1ebe50
yoshi <yn55@njit.edu>
2022-05-05 19:14:27 -0400

    Test 14. testing subtraction of calculator

--- tests/calculator.py
+++ tests/calculator.py
@@ -25,3 +25,8 @@ def test_calculator_add_method():
     """Testing the Calculator"""
     calculator = Calculator()
     assert calculator.add(1) == 1
+
+def test_calculator_subtract_method():
+    """Testing the Calculator Subtract"""
+    calculator = Calculator()
+    assert calculator.subtract(1) == -1
