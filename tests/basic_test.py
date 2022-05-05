1df6cb2ad1be948f0a5a3dd95ca719016355ff67
yoshi <yn55@njit.edu>
2022-05-05 18:27:29 -0400

    Test 6. Testing word index in Index page in the Index Client

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -29,3 +29,9 @@ def test_request_main_menu_links(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b'href="/register"' in response.data
+
+def test_request_index(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Index" in response.data
