affaa0dd918312ff04c9f3942aca85ad699a49f0
yoshi <yn55@njit.edu>
2022-05-05 19:08:55 -0400

    Test 13. testing result of calculator

--- tests/calculator.py
+++ tests/calculator.py
@@ -12,3 +12,16 @@ def test_calculator_get_result_method():
     calculator = Calculator()
     assert calculator.get_result() == 0
 
+def test_calculator_result_property():
+    """Testing the Calculator"""
+    calc1 = Calculator()
+    calc2 = Calculator()
+    calc1.result = 5
+    calc2.result = 6
+    assert calc1.result == 5
+    assert calc2.result == 6
+
+def test_calculator_add_method():
+    """Testing the Calculator"""
+    calculator = Calculator()
+    assert calculator.add(1) == 1
