13ca0481be4a9ba79c1e2b762160be3af9875d08
yoshi <yn55@njit.edu>
2022-05-05 18:08:13 -0400

    Test 5. Testing register page in the Index Client

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -23,3 +23,9 @@ def test_request_main_menu_links(client):
     response = client.get("/")
     assert response.status_code == 200
     assert b'href="/login"' in response.data
+
+def test_request_main_menu_links(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b'href="/register"' in response.data
