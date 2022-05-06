f5446b7797a2acae56e47b3e4462d955f8c699e9
yoshi <yn55@njit.edu>
2022-05-05 20:16:56 -0400

    Test 22. Testing auth. login and register

---
+++ tests/auth_test.py
@@ -0,0 +1,6 @@
+def test_request_main_menu_links(client):
+    """This makes the index page"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b'href="/login"' in response.data
+    assert b'href="/register"' in response.data
