e16cc1f73d9884e2f2499b795d13f948edaa4ecd
yoshi <yn55@njit.edu>
2022-05-05 20:02:39 -0400

    Test 16. Testing production configuration

--- tests/config_test.py
+++ tests/config_test.py
@@ -18,3 +18,8 @@ def test_testing_config(application):
     assert application.config['TESTING']
     assert not application.config['PRESERVE_CONTEXT_ON_EXCEPTION']
     assert application.config['SQLALCHEMY_DATABASE_URI'] == 'sqlite:///:memory:'
+
+def test_production_config(application):
+    application.config.from_object('app.config.ProductionConfig')
+    assert not application.config['DEBUG']
+    assert not application.config['TESTING']
